44407235b342557356face57bd5676f030c48f4d
salvador.sanjuan@gmail.com <salvador.sanjuan@gmail.com>
2014-09-03 02:35:10 +0200

    modified:   test_class_variables.py

--- test_class_variables.py
+++ test_class_variables.py
@@ -1,4 +1,9 @@
 from persistent import Persistent
+from ZODB import FileStorage, DB
+import transaction
+
+storage = FileStorage.FileStorage('mydatabase.fs')
+db = DB(storage)
 
 class Test_variables(Persistent):
 
@@ -15,38 +20,9 @@ class Test_variables(Persistent):
 			
 		
 	def connect_ZODB(self):
-
-		#import persistent
-		from ZODB import FileStorage, DB
-		storage = FileStorage.FileStorage('mydatabase.fs')
-		db = DB(storage)
+		
 		connection = db.open()
 		root = connection.root()
-		
-		# print ( "Lo que hay en root: ", root.items())
-		# print type(root)
-		
-		
-		# if not root.catalogue:
-			# root.catalogue = {}
-			# for job in self.catalogue.keys():
-				# print job, self.catalogue[job]
-				# root.catalogue[job] = self.catalogue[job]
-		# else:
-			# for job in self.catalogue.keys():
-				# root.catalogue[job] = self.catalogue[job]
-			
-			
-		# print("Variable catalogo de la clase " ,self.catalogue.keys())	
-		
-		# print("Diccionario ZODB " , root.catalogue)
-		
-		# print type(root)
-		# print type(root.catalogue)
-			
-		# import transaction
-		# transaction.commit()
-		# connection.close()
 			
 		return connection, root
 		
@@ -64,11 +40,10 @@ class Test_variables(Persistent):
 			self.catalogue[position] = gross_salary
 		
 		#opens a connection to ZODB to make the changes permament
-		connection, root = connect_ZODB()
+		connection, root = self.connect_ZODB()
 		
 		root.catalogue = self.catalogue
-		
-		import transaction
+
 		transaction.commit()
 		connection.close()
 			
@@ -79,22 +54,16 @@ if __name__ == "__main__":
 	
 	print catalogue.catalogue
 	
-	catalogue.add_job('juerguista',20000)
-	catalogue.add_job('secretario', 15000)
-	catalogue.add_job('zorro', 150000000)
+	# catalogue.add_job('juerguista',20000)
+	# catalogue.add_job('secretario', 15000)
+	# catalogue.add_job('zorro', 150000000)
 	
-	print catalogue.catalogue
+	# print catalogue.catalogue
 	
 	# connection, root = catalogue.connect_ZODB()
 	
-	# import transaction
-	# transaction.commit()
-	
-	
-	
-	
-	# print("Diccionario root fuera de la funcion " , root.catalogue.items())
-	
+
+
 	
 	
 					
